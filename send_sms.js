fcd29da9ed9c580e4a74a411c1d34f92de538233
Elvin Liu <69759405+Elvin-Liu237@users.noreply.github.com>
2022-07-22 17:31:46 -0400

    Add files via upload
    Working code.

--- send_sms.js
+++ send_sms.js
@@ -11,30 +11,52 @@ const client = require('twilio')(accountSid, authToken);
 
 //array for all phone numbers goes here.
 
-//const numbers = ['+17046993417', '+15712508955','+13365080858'];
-const numbers = ['+17046993417'];
+//const numbers = ['+17046993417', '+15712508955','+13365080858', '+13308613678', '+19806218983', '+19806218982', '+17046996482', '+19193586136', '+17049151801', '+15167219196'];
+
+/*var fs = require('fs');
+
+console.log('Executed Before File Reading');
+const numbers = [];
+fs.readFile('./clientsfile.txt', 'utf8', function(error, data) {
+    //console.log(data);
+    //numbers.push(data);
+    if(error) throw error;
+    var phonenumbers = data.toString().split("\n");
+    for (i in phonenumbers) {
+      console.log(phonenumbers);
+      numbers.push(phonenumbers[i]);
+    }
+   
+});*/
+
+console.log('Executed Before File Reading');
+const fs = require("fs"); 
+var data = fs.readFileSync("clientsfile.txt"); 
+var dataArr = data.toString('utf8').split("\n");  
+console.log(dataArr); 
+
+const numbers = dataArr;
+
+console.log('Executed After File Reading, probably');
+//console.log(numbers)
+
 Promise.all (
   numbers.map(number => {
     return client.messages.create({
-     //body: 'This is Elvin, and I am just testing HackNC`s Twilio. I promise this is not a scam.',
-     //body: 'Come to the great hall for Spaghetti!',
-
      //githubusername.io
      //body: '10 minutes left!',
      //body: 'We are at the 5 Minute Warning!',
      //body: 'Come to the great hall for Spaghetti!',
      //body: 'ATTENTION! 30 minutes left till judgment!'
-     //body: 'Judgment time!'
+     body: 'Judgment time!',
      //body: Come to the great hall for a java workshop!
      //body: Come to the great hall for a python workshop to create a random number generator!
-     body: 'Come to the great hall for Dim Sum!',
+     //body: 'Testing one, two, three! Come to the great hall! BTW This is Elvin from HackNC, not a hacker, text me if you got this message if you are able to atm haha.', 
+     //from: '+13254253605',
+     from: '+19362376004',
      
      
-     
-
-     from: '+13254253605',
-     //use a forloop around the entire client.messages to send sms to multiple numbers
-     to: number
+     to: number,
     });
    })
 )
